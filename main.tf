6d422944245ab3ad2f5d3eb0723dabb02c1597d8
gotta999 <64887611+gotta999@users.noreply.github.com>
2025-03-30 12:56:10 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,96 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "simple-time-service-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_ecs_cluster" "simple_time_cluster" {
+  name = "simple-time-cluster"
+}
+
+resource "aws_ecs_task_definition" "simple_time_task" {
+  family                   = "simple-time-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = var.ecs_cpu
+  memory                   = var.ecs_memory
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "simple-time-service"
+      image     = "your-dockerhub-username/simpletimeservice:latest"
+      cpu       = var.ecs_cpu
+      memory    = var.ecs_memory
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "simple_time_service" {
+  name            = "simple-time-service"
+  cluster         = aws_ecs_cluster.simple_time_cluster.id
+  task_definition = aws_ecs_task_definition.simple_time_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = module.vpc.private_subnets
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    container_name   = "simple-time-service"
+    container_port   = var.container_port
+  }
+
+  desired_count = 1
+}
+
+resource "aws_lb" "ecs_lb" {
+  name               = "simple-time-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+  name        = "simple-time-tg"
+  port        = var.container_port
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.ecs_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
+}
